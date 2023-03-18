d774c1d389f13d1143bf11d54b504b46973fb32c
Alexander <72211641+agriff93@users.noreply.github.com>
2023-03-18 14:56:20 -0400

    Add files via upload

--- tests/unit/test_get_movie_by_title.py
+++ tests/unit/test_get_movie_by_title.py
@@ -1,1 +1,12 @@
 # TODO: Feature 3
+from src.repositories.movie_repository import MovieRepository
+from src.models.movie import Movie
+
+def test_get_movie_by_title():
+    movies = MovieRepository()
+    assert None == movies.get_movie_by_title("Star Wars")
+    movies.create_movie("Star Wars", "George Lucas", 5)
+    foundTitle = movies.get_movie_by_title("Star Wars")
+    assert foundTitle.title == "Star Wars"
+    assert foundTitle.director == "George Lucas"
+    assert foundTitle.rating == 5
